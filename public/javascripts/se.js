9e45ef7dcdf192d543bf508453e6c862cf0871c1
JIMITAS-805\Nonaka <nonajun805@gmail.com>
2022-06-06 20:50:23 +0900

    howl

--- public/javascripts/se.js
+++ public/javascripts/se.js
@@ -1,11 +1,130 @@
 //コンテンツ共通の効果音の登録
-const pi = new Audio("../Sounds/pi.mp3");
-const set = new Audio("../Sounds/set.mp3");
-const seikai1 = new Audio("../Sounds/seikai.mp3");
-const seikai2 = new Audio("../Sounds/seikai2.mp3");
-const reset = new Audio("../Sounds/reset.mp3");
-const right = new Audio("../Sounds/right.mp3");
-const move1 = new Audio("../Sounds/move1.mp3");
-const move2 = new Audio("../Sounds/move2.mp3");
-const alert = new Audio("../Sounds/alert.mp3");
-const kako = new Audio("../Sounds/kako.mp3");
+const pi = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/pi.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const set = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/set.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const seikai1 = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/seikai.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const seikai2 = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/seikai2.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const reset = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/reset.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const right = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/right.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const move1 = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/move1.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const move2 = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/move2.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const alert = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/alert.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
+
+const kako = new Howl({
+  //読み込む音声ファイル
+  src: ["../Sounds/kako.mp3"],
+
+  // 設定 (以下はデフォルト値です)
+  preload: true, // 事前ロード
+  volume: 1.0, // 音量(0.0〜1.0の範囲で指定)
+  loop: false, // ループ再生するか
+  autoplay: false, // 自動再生するか
+
+  
+});
